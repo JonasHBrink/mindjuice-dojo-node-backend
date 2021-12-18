5f21c52f7e106b5c89d1c5f9f100695922b1bffb
Jonas Høy Brink <jhb@proactive.dk>
2021-12-18 10:17:13 +0100

    Fix

--- server/index.js
+++ server/index.js
@@ -8,29 +8,27 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-const ftp = require("basic-ftp")
+// const ftp = require("basic-ftp")
 
-example()
-
-app.upload(() => {
-  const client = new ftp.Client()
-  client.ftp.verbose = true
-  try {
-    await client.access({
-      host: "linux110.unoeuro.com",
-      user: "studiebyen-humle.dk",
-      password: "Crockstar612",
-      secure: true
-    })
-    console.log(await client.list())
-    await client.uploadFrom("README.md", "README_FTP.md")
-    await client.downloadTo("README_COPY.md", "README_FTP.md")
-  }
-  catch (err) {
-    console.log(err)
-  }
-  client.close()
-});
+// async function upload(file) {
+//   const client = new ftp.Client()
+//   client.ftp.verbose = true
+//   try {
+//     await client.access({
+//       host: "linux110.unoeuro.com",
+//       user: "studiebyen-humle.dk",
+//       password: "Crockstar612",
+//       secure: true
+//     })
+//     console.log(await client.list())
+//     await client.uploadFrom(file, "jonas")
+//     // await client.downloadTo("README_COPY.md", "README_FTP.md")
+//   }
+//   catch (err) {
+//     console.log(err)
+//   }
+//   client.close()
+// }
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
