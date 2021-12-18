763544bc682011a561133579a3ca681d458cda51
Jonas Høy Brink <jhb@proactive.dk>
2021-12-18 10:39:52 +0100

    Upload

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 const cors = require('cors');
+const ftp = require("basic-ftp");
 
 app.use(cors({
   origin: '*'
@@ -14,30 +13,29 @@ app.get("/api", (req, res) => {
 });
 
 app.post("/upload", (req, res) => {
-  res.json({ message: req });
+  upload(req);
+  res.json({ message: "Upload" });
 });
 
-// const ftp = require("basic-ftp")
-
-// async function upload(file) {
-//   const client = new ftp.Client()
-//   client.ftp.verbose = true
-//   try {
-//     await client.access({
-//       host: "linux110.unoeuro.com",
-//       user: "studiebyen-humle.dk",
-//       password: "Crockstar612",
-//       secure: true
-//     })
-//     console.log(await client.list())
-//     await client.uploadFrom(file, "jonas")
-//     // await client.downloadTo("README_COPY.md", "README_FTP.md")
-//   }
-//   catch (err) {
-//     console.log(err)
-//   }
-//   client.close()
-// }
+async function upload(file) {
+  const client = new ftp.Client()
+  client.ftp.verbose = true
+  try {
+    await client.access({
+      host: "linux110.unoeuro.com",
+      user: "studiebyen-humle.dk",
+      password: "Crockstar612",
+      secure: true
+    })
+    console.log(await client.list())
+    await client.uploadFrom(file, "jonas")
+    // await client.downloadTo("README_COPY.md", "README_FTP.md")
+  }
+  catch (err) {
+    console.log(err)
+  }
+  client.close()
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
